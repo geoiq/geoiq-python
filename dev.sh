5b21a85445638ef70abc60031aeec6ab005cccc5
Dan Shoutis <dan@shoutis.org>
2011-04-05 23:23:52 -0600

    Tweak to venv.

--- dev.sh
+++ dev.sh
@@ -8,7 +8,9 @@ if [ ! -e scratch/venv-geoiq ]; then
     virtualenv --no-site-packages scratch/venv-geoiq
     scratch/venv-geoiq/bin/python setup.py develop
     scratch/venv-geoiq/bin/easy_install ipython
+    scratch/venv-geoiq/bin/easy_install shapely
 fi
+
 OLD_PS1="$PS1"
 . scratch/venv-geoiq/bin/activate
 PS1="\[\033[0;32m\] (geoiq) \[\033[0m\] $OLD_PS1"
