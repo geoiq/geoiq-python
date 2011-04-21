ab33a23c2ea25ce4c20efd43b95efc706e23def9
unknown <dan@.(none)>
2011-04-21 13:05:48 -0600

    Tweak to only generate venv if needed; otherwise just activate it.

--- windev.bat
+++ windev.bat
@@ -1,3 +1,10 @@
 
+if exist "scratch\venv-geoiq" goto cont
+
 C:\Python25\Scripts\virtualenv.exe "scratch\venv-geoiq"
-scratch\venv-geoiq\bin\python setup.py
+scratch\venv-geoiq\Scripts\python.exe setup.py
+scratch\venv-geoiq\Scripts\easy_install.exe ipython
+
+:cont
+
+scratch\venv-geoiq\Scripts\activate.bat
